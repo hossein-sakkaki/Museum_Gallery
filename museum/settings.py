2e3e9a93ae0b2a5fdc54ff3e3818af5926ca167f
Hossein Sakkaki <100998685+hossein-sakkaki@users.noreply.github.com>
2023-03-17 11:23:49 +0300

    Update settings.py

--- museum/settings.py
+++ museum/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)#y2!+d5&!=1&fp_s=emtk0tk^g+_d+s96v@i+5xe1662)@%c'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '12348765',
+        'PASSWORD': '',
     }
 }
 
